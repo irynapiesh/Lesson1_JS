3ad80406af1e9e57ada349d616c6b78aebf6c9e8
OlehKolodochka <kolodockaoleg@gmail.com>
2023-08-16 15:54:30 +0300

    add main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+let a1;
+a1 = 'hello';
+console.log(a1);
+
+let a2;
+a2 = 'owu';
+console.log(a2);
+
+let a3;
+a3 = 'com';
+console.log(a3);
+
+let a4;
+a4 = 'ua';
+console.log(a4);
+
+let n1 = 1;
+console.log(n1);
+
+let n2 = 10;
+console.log(n2);
+
+let n3 = -999;
+console.log(n3);
+
+let n4 = 123;
+console.log(n4);
+
+let n5 = 3.14;
+console.log(n5);
+
+let n6 = 2.7;
+console.log(n6);
+
+let n7 = 16;
+console.log(n7);
+
+let b1 = 10;
+let b2 = 100;
+let bRes = b1<b2;
+console.log(bRes);
+
+let b3 = 4;
+let b4 = 8;
+let bResult = b1>b2;
+console.log(bResult);
+
+let firstName = 'Iryna ';
+let middleName = 'Petrivna ';
+let lastName = 'Pieshko';
+let person = firstName + middleName + lastName;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+let name = prompt('Ірина');
+console.log(name);
+
+
+
